77dffb78e6c4e3e8b5d4bd122ed58d54a8733d41
zookeroro <zookeroro@gmail.com>
2020-05-04 11:08:16 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import './App.css';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import NavBar from './components/NavBar'
 function App() {
 	return (
 		<BrowserRouter>
-			<Switch>
-				<Route path="/">
-					<HomePage />
-				</Route>
-			</Switch>
+			<div>
+        <NavBar/>
+				<Switch>
+					<Route path="/">
+						<HomePage />
+					</Route>
+				</Switch>
+			</div>
 		</BrowserRouter>
 	);
 }
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import './navStyle.css'
 export default () => {
     return (
-        <div>
-            
+        <div className="nav-conatiner">
+           
         </div>
     )
 }
